c286f6c2e1c6ee05f365dd6bafebc063a1fedfc2
Guilherme Silva <133605425+GuilhermeSP01@users.noreply.github.com>
2025-02-15 22:27:21 -0300

    Implementação de UserDetailsService e Service

---
+++ src/main/java/br/unip/projeto_clinica/user/UserPrincipal.java
@@ -0,0 +1,31 @@
+package br.unip.projeto_clinica.user;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class UserPrincipal implements UserDetails {
+
+    private final User user;
+
+    public UserPrincipal(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return user.getRoles();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+}
