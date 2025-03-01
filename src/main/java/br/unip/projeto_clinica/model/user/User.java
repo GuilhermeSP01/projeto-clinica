33596343acf5f6bcdec3a7d88619562d9cc07ffe
Guilherme Silva <133605425+GuilhermeSP01@users.noreply.github.com>
2025-03-01 00:14:06 -0300

    Reformulação da classe User

--- src/main/java/br/unip/projeto_clinica/model/user/User.java
+++ src/main/java/br/unip/projeto_clinica/model/user/User.java
@@ -1,59 +1,82 @@
 package br.unip.projeto_clinica.model.user;
 
-import jakarta.persistence.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.security.core.GrantedAuthority;
 
 import java.util.Collection;
-import java.util.UUID;
 
-@Entity
-@Table(name = "USERS")
+@Document
 public class User {
 
-    @Id @GeneratedValue(strategy = GenerationType.UUID)
-    private UUID id;
-
-    @Column(unique = true, nullable = false)
-    private String username;
-
-    @Column(nullable = false)
-    private String password;
-
-    @Column
+    @Id
+    private String id;
+    private String cpf;
+    private String nome;
+    private String telefone;
+    private String email;
+    private String senha;
     private Collection<? extends GrantedAuthority> roles;
 
     public User() {
     }
 
-    public User(UUID id, String username, String password, Collection<? extends GrantedAuthority> roles) {
+    public User(String id, String cpf, String nome, String telefone, String email, String senha, Collection<? extends GrantedAuthority> roles) {
         this.id = id;
-        this.username = username;
-        this.password = password;
+        this.cpf = cpf;
+        this.nome = nome;
+        this.telefone = telefone;
+        this.email = email;
+        this.senha = senha;
         this.roles = roles;
     }
 
-    public UUID getId() {
+    public String getId() {
         return id;
     }
 
-    public void setId(UUID id) {
+    public void setId(String id) {
         this.id = id;
     }
 
-    public String getUsername() {
-        return username;
+    public String getCpf() {
+        return cpf;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
     }
 
-    public String getPassword() {
-        return password;
+    public String getNome() {
+        return nome;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
     }
 
     public Collection<? extends GrantedAuthority> getRoles() {
@@ -67,10 +90,14 @@ public class User {
     @Override
     public String toString() {
         return "User{" +
-                "id=" + id +
-                ", username='" + username + '\'' +
-                ", password='" + password + '\'' +
-                ", roles=" + roles.stream().findFirst().get().getAuthority() +
+                "id='" + id + '\'' +
+                ", cpf='" + cpf + '\'' +
+                ", nome='" + nome + '\'' +
+                ", telefone='" + telefone + '\'' +
+                ", email='" + email + '\'' +
+                ", senha='" + senha + '\'' +
+                ", roles=" + roles +
                 '}';
     }
+    
 }
